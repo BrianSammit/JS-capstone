adb8efc5fda25a4c3cc4cbe7a05d05650a18f3d2
BrianSammit <briansammit.cruz@gmail.com>
2021-04-27 16:59:55 -0500

    Add character, ground and jump action

---
+++ src/game.js
@@ -0,0 +1,37 @@
+class Game extends Phaser.Scene {
+  constructor() {
+    super({ key: "game" });
+  }
+
+  preload() {
+    this.load.image("ground", "../dist/assets/ground.png");
+    this.load.image("zombie-1", "../dist/assets/animation/Run3.png");
+  }
+
+  create() {
+    this.gameSpeed = 10;
+    const { height, width } = this.game.config;
+
+    this.ground = this.add
+      .tileSprite(0, height, width, 0, "ground")
+      .setOrigin(0, 1);
+    this.ground.scale = 0.5;
+    this.zombie = this.physics.add
+      .sprite(0, height, "zombie-1")
+      .setOrigin(0, 1)
+      .setCollideWorldBounds(true)
+      .setGravityY(5000);
+    this.zombie.scale = 0.3;
+    this.input.keyboard.on("keydown-SPACE", this.jump, this);
+  }
+
+  jump() {
+    this.zombie.setVelocityY(-1600);
+  }
+
+  update() {
+    this.ground.tilePositionX += this.gameSpeed;
+  }
+}
+
+export default Game;
